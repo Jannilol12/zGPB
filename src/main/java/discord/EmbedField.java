23e762584bfd24db673836778fc70630854e6a92
BuildTools <58337328+stormofice@users.noreply.github.com>
2021-03-15 03:02:16 +0100

    Added embeds to MessageCrafter, this required enabling preview features

---
+++ src/main/java/discord/EmbedField.java
@@ -0,0 +1,5 @@
+package discord;
+
+public record EmbedField(String key, String value, boolean inline) {
+
+}
--- src/main/java/discord/MessageCrafter.java
+++ src/main/java/discord/MessageCrafter.java
@@ -1,6 +1,32 @@
 package discord;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.awt.*;
+import java.time.Instant;
+import java.util.Set;
+
 public class MessageCrafter {
+
+    public static MessageEmbed craftGenericEmbedMessage(String title, Set<EmbedField> fields) {
+        return craftGenericEmbedMessage(title, fields.toArray(new EmbedField[]{}));
+    }
+
+    public static MessageEmbed craftGenericEmbedMessage(String title, EmbedField... fields) {
+        EmbedBuilder eb = new EmbedBuilder();
+
+        eb.setColor(Color.CYAN);
+        eb.setTimestamp(Instant.now());
+        eb.setFooter("[zGBP]");
+        eb.setTitle(title);
+
+        for (EmbedField ef : fields)
+            eb.addField(ef.key(), ef.value(), ef.inline());
+
+        return eb.build();
+    }
+
 
     public static String craftCodeMessage(String language, String content, boolean truncate) {
         int maxAllowedLength = 1999 - language.length() - 6;
