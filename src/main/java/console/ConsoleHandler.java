f88727128e8d431dd1a5a3148a628480b8d112e3
stormofice <58337328+stormofice@users.noreply.github.com>
2021-04-02 21:40:42 +0200

    waiting for input is now done in a separate thread

--- src/main/java/console/ConsoleHandler.java
+++ src/main/java/console/ConsoleHandler.java
@@ -7,23 +7,24 @@ import java.util.Scanner;
 public class ConsoleHandler {
 
     public void checkInput() {
+        new Thread(() -> {
+            Scanner consoleScanner = new Scanner(System.in);
 
-        Scanner consoleScanner = new Scanner(System.in);
+            while (consoleScanner.hasNextLine()) {
 
-        while (consoleScanner.hasNextLine()) {
+                String currentLine = consoleScanner.nextLine();
 
-            String currentLine = consoleScanner.nextLine();
+                if (currentLine.equals("quit"))
+                    System.exit(0);
+                else if (currentLine.equals("dump"))
+                    JADB.INSTANCE.databaseHandler.dumpDatabase();
+                else if (currentLine.equals("dimd"))
+                    JADB.INSTANCE.gradeListener.isEnabled = false;
+                else
+                    System.out.println(JADB.INSTANCE.databaseHandler.getResultFromQuery(currentLine, false));
+            }
 
-            if (currentLine.equals("quit"))
-                System.exit(0);
-            else if (currentLine.equals("dump"))
-                JADB.INSTANCE.databaseHandler.dumpDatabase();
-            else if (currentLine.equals("dimd"))
-                JADB.INSTANCE.gradeListener.isEnabled = false;
-            else
-                System.out.println(JADB.INSTANCE.databaseHandler.getResultFromQuery(currentLine, false));
-        }
-
+        }).start();
     }
 
 }
